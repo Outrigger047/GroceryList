cf6576c055d6e7a51a101f7e426611b4d2bbc344
Outrigger047-HOME <andrew.wyshak@gmail.com>
2016-07-20 19:31:22 -0400

    Added icon for real this time.

---
+++ GroceryList.Main/GroceryRepoItem.cs
@@ -0,0 +1,20 @@
+﻿namespace GroceryList.Main
+{
+    public class GroceryRepoItem : GroceryItem
+    {
+        public bool IsOnList { get; private set; }
+
+        public GroceryRepoItem(string nameIn) : base(nameIn)
+        {
+        }
+
+        public GroceryRepoItem(string nameIn, int priceIn, Enums.Stores storeIn) : base(nameIn, priceIn, storeIn)
+        {
+        }
+
+        public void SetIsOnListFlag(bool statusIn)
+        {
+            IsOnList = statusIn;
+        }
+    }
+}
