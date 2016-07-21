2c65cbdb0bc3c903c4a7d7539bf76ba782f1d9c8
Outrigger047-HOME <andrew.wyshak@gmail.com>
2016-07-20 20:10:03 -0400

    Incremental changes

--- GroceryList.Main/GroceryItemRepository.cs
+++ GroceryList.Main/GroceryItemRepository.cs
@@ -8,14 +8,9 @@ namespace GroceryList.Main
 {
     public class GroceryItemRepository
     {
-        private const int DISK_REPO_HANNAFORD_COL_INDEX = 2;
-        private const int DISK_REPO_SHAWS_COL_INDEX = 3;
-        private const int DISK_REPO_SAMS_COL_INDEX = 4;
-        private readonly string DISK_REPO_FILE_PATH = 
-            Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath), 
-                @"\Assets\repo.txt");
-        private readonly string DISK_REPO_VALID_HEADER_ROW = "ITEM\tCATEGORY\tHANNAFORD_PRICE\tSHAWS_PRICE\tSAMS_PRICE";
-
+        private static readonly string DISK_REPO_FILE_PATH = 
+            Path.Combine(Path.GetDirectoryName(
+                new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath), @"\Assets\repo.txt");
         private readonly FileInfo _file;
 
         public List<GroceryRepoItem> Items { get; private set; }
@@ -23,9 +18,7 @@ namespace GroceryList.Main
         public GroceryItemRepository()
         {
             _file = new FileInfo(DISK_REPO_FILE_PATH);
-            Items = new List<GroceryRepoItem>();
-
-            LoadRepositoryFromDisk();
+            Items = DiskRepoHelpers.LoadRepositoryFromDiskNew(_file);
         }
 
         public GroceryItem GetItemFromString(string itemToFind)
@@ -41,7 +34,7 @@ namespace GroceryList.Main
             return null;
         }
 
-        [System.Obsolete("Use LoadRepositoryFromDiskNew()")]
+        [System.Obsolete("Use GroceryList.Main.Helpers.DiskRepoHelpers.LoadRepositoryFromDiskNew()")]
         private void LoadRepositoryFromDisk()
         {
             bool theOldCollegeTry = true;
@@ -76,56 +69,6 @@ namespace GroceryList.Main
                     // Give it the old college try!
                 } 
             }
-        }
-
-        private void LoadRepositoryFromDiskNew()
-        {
-            bool tryAgain;
-
-            do
-            {
-                tryAgain = false;
-
-                try
-                {
-                    StreamReader sr = new StreamReader(_file.FullName);
-
-                    while (!sr.EndOfStream)
-                    {
-                        string[] currentLine = sr.ReadLine().Split('\t');
-
-                        if (currentLine.ToString() == DISK_REPO_VALID_HEADER_ROW)
-                        {
-
-                        }
-                        else
-                        {
-                            throw new Exception("Header row invalid");
-                        }
-
-                        //if (currentLine[1] == "" && currentLine[2] == "")
-                        //{
-                        //    Items.Add(new GroceryRepoItem(currentLine[0]));
-                        //}
-                        //else
-                        //{
-                        //    Items.Add(new GroceryRepoItem(currentLine[0],
-                        //        MoneyShit.ParseMoneysFromFile(currentLine[1]),
-                        //        ParseStoreName(currentLine[2])));
-                        //}
-                    }
-                }
-                catch (FileNotFoundException)
-                {
-                    tryAgain = true;
-
-                    FileStream fs = File.Create(_file.FullName);
-                    StreamWriter sw = new StreamWriter(fs);
-                    sw.WriteLine(DISK_REPO_VALID_HEADER_ROW);
-                    sw.Close();
-                    fs.Close();
-                }
-            } while (tryAgain);
         }
 
         private void WriteRepositoryToDisk()
--- GroceryList.Main/GroceryList.Main.Helpers/DiskRepoHelpers.cs
+++ GroceryList.Main/GroceryList.Main.Helpers/DiskRepoHelpers.cs
@@ -1,7 +1,71 @@
-﻿namespace GroceryList.Main.Helpers
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace GroceryList.Main.Helpers
 {
     public static class DiskRepoHelpers
     {
+        private const int DISK_REPO_HANNAFORD_COL_INDEX = 2;
+        private const int DISK_REPO_SHAWS_COL_INDEX = 3;
+        private const int DISK_REPO_SAMS_COL_INDEX = 4;
 
+        private static readonly string DISK_REPO_VALID_HEADER_ROW = 
+            "ITEM\tCATEGORY\tHANNAFORD_PRICE\tSHAWS_PRICE\tSAMS_PRICE";
+
+        public static List<GroceryRepoItem> LoadRepositoryFromDiskNew(FileInfo path)
+        {
+            List<GroceryRepoItem> repoFromDisk = new List<GroceryRepoItem>();
+
+            bool tryAgain;
+
+            do
+            {
+                tryAgain = false;
+
+                try
+                {
+                    StreamReader sr = new StreamReader(path.FullName);
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] currentLine = sr.ReadLine().Split('\t');
+
+                        if (currentLine.ToString() == DISK_REPO_VALID_HEADER_ROW)
+                        {
+
+                        }
+                        else
+                        {
+                            throw new Exception("Header row invalid");
+                        }
+
+                        //if (currentLine[1] == "" && currentLine[2] == "")
+                        //{
+                        //    Items.Add(new GroceryRepoItem(currentLine[0]));
+                        //}
+                        //else
+                        //{
+                        //    Items.Add(new GroceryRepoItem(currentLine[0],
+                        //        MoneyShit.ParseMoneysFromFile(currentLine[1]),
+                        //        ParseStoreName(currentLine[2])));
+                        //}
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    tryAgain = true;
+
+                    FileStream fs = File.Create(path.FullName);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine(DISK_REPO_VALID_HEADER_ROW);
+                    sw.Close();
+                    fs.Close();
+                }
+            } while (tryAgain);
+
+            return repoFromDisk;
+        }
     }
 }
