1fa7de369c839ec62aaa096dd8478dad49e1b2eb
Outrigger047-HOME <andrew.wyshak@gmail.com>
2016-07-30 11:28:48 -0400

    Updated static money stuff

--- GroceryList.Main/MoneyShit.cs
+++ GroceryList.Main/MoneyShit.cs
@@ -15,5 +15,15 @@ namespace GroceryList.Main
                 return Convert.ToInt32(Math.Round(Convert.ToDouble(moneysFromFile), 2) * 100);
             }
         }
+
+        public static decimal PenniesToDecimal(int penniesIn)
+        {
+            return Math.Round((decimal)penniesIn / 100, 2);
+        }
+
+        public static int DecimalToPennies(decimal decimalIn)
+        {
+            return Convert.ToInt32(decimalIn * 100);
+        }
     }
 }
